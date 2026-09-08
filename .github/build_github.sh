e9fb3b60ba07f5eea87c7244193c94c76abea6bb
zxsctxx <zxsctxx@users.noreply.github.com>
2026-09-08 19:41:34 +0800

    feat: use SideStore with 10.7.0.1 fallback from zxsctxx release

--- .github/build_github.sh
+++ .github/build_github.sh
@@ -41,7 +41,7 @@ mv ./tmp/SideStoreSupport.framework Payload/LiveContainer.app/Frameworks
 
 # download SideStore
 cd tmp
-wget https://github.com/LiveContainer/SideStore/releases/download/nightly/SideStore.ipa
+wget https://github.com/zxsctxx/SideStore-LC/releases/download/sidestore-fallback-test/SideStore.ipa
 unzip SideStore.ipa
 cd ..
 
